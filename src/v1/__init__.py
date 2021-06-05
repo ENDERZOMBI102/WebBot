1c5effe6b67e655bf27adee956d440cc51f367c8
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-06-06 01:45:55 +0200

    members -> users

--- src/v1/__init__.py
+++ src/v1/__init__.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from . import messages, members
+from . import messages, users
 
 router = APIRouter(
 	prefix="/v1"
 )
 
 router.include_router( messages.router )
-router.include_router( members.router )
+router.include_router( users.router )
